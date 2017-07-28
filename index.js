64d3790369b36c7aff8a746c7c343f796a28b685
jeremylenz <jeremylenz@gmail.com>
2017-07-28 08:58:38 -0400

    FIRST

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '1d6968f60c2234d2da8539205ee68b86e62ce1f9'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
